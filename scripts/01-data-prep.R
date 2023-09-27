caacf0f3e8e04ab829ff4430953cc4c46b7110af
Francisca <frcastrog@uc.cl>
2023-09-27 12:31:05 +0200

    changes 27.09

--- scripts/01-data-prep.R
+++ scripts/01-data-prep.R
@@ -1,10 +1,10 @@
 #-------------------------------Data Preparation-------------------------------#
 #-Author: Francisca Castro ----------------------- Created: September 18, 2023-#
-#-R Version: 4.3.1 ------------------------------- Revised: September 19, 2023-#
+#-R Version: 4.3.1 ------------------------------- Revised: September 27, 2023-#
 
-# 1) Load necessary packages
+# 1) Load packages
 
-pacman::p_load(haven, dplyr, tidyr, magrittr)
+pacman::p_load(haven, dplyr, tidyr, magrittr, forcats)
 
 # 2) Load data
 
@@ -14,109 +14,124 @@ str(data_raw)
 
 names(data_raw)
 
-## Data cleaning
+# 3) Data cleaning
 
 ### NAs replacement
 # Some of the varaibles have `__NA__` instead of normal NA, so it's necessary to
 # change that. 
 
-data_clean <- data_raw %>%
+data_raw %<>%
   mutate_if(is.character, ~ ifelse(. == "__NA__", NA, .)) 
 
-# 3) Create new variables of experimental treatments/policy positions
+### Keep only necessary variables
+# So far: caseid, teamweight, birthyr, educ, race, CC21_330a (self ideology),
+# pid3 (3 point party ID), pid7 (7 point party id), gender4, BGU_Trumpapproval,
+# CC21_315a (Biden approval).
+# Plus the variable on the treatment BGU_group
+# and all the variables related to policy support: 
+#"BGU_control1"          "BGU_control2"          "BGU_control3"          "BGU_control4"          "BGU_control5"         
+# "BGU_control6"          "BGU_control7"          "BGU_control8"          "BGU_control9"          "BGU_Trumplib1"        
+# "BGU_Trumplib2"         "BGU_Trumplib3"         "BGU_Trumplib4"         "BGU_Trumplib5"         "BGU_Trumplib6"        
+# "BGU_Trumplib7"         "BGU_Trumplib8"         "BGU_Trumplib9"         "BGU_Trumpcon1"         "BGU_Trumpcon2"        
+# "BGU_Trumpcon3"         "BGU_Trumpcon4"         "BGU_Trumpcon5"         "BGU_Trumpcon6"         "BGU_Trumpcon7"        
+# "BGU_Trumpcon8"         "BGU_Trumpcon9"         "BGU_friendlib1"        "BGU_friendlib2"        "BGU_friendlib3"       
+# "BGU_friendlib4"        "BGU_friendlib5"        "BGU_friendlib6"        "BGU_friendlib7"        "BGU_friendlib8"       
+# "BGU_friendlib9"        "BGU_friendcon1"        "BGU_friendcon2"        "BGU_friendcon3"        "BGU_friendcon4"       
+# "BGU_friendcon5"        "BGU_friendcon6"        "BGU_friendcon7"        "BGU_friendcon8"        "BGU_friendcon9"    
 
-# Given that the policy positions are constructed giving NAs to all of the individuals
-# who didn't responded that prompt given that they were in a different experimental
-# treatment, in order to conduct further comparisons is necessary to combine
-# the policy positions. Nine new variables are created for each policy positions,
-# and for each position, the experimental treatments are combined
+data_filtered <- data_raw %>%
+  select(caseid, teamweight, birthyr, educ, race, CC21_330a, pid3, pid7, gender4, 
+         BGU_Trumpapproval, CC21_315a, BGU_group,
+         BGU_control1, BGU_control2, BGU_control3, BGU_control4, BGU_control5,
+         BGU_control6, BGU_control7, BGU_control8, BGU_control9, BGU_Trumplib1,
+         BGU_Trumplib2, BGU_Trumplib3, BGU_Trumplib4, BGU_Trumplib5, BGU_Trumplib6,
+         BGU_Trumplib7, BGU_Trumplib8, BGU_Trumplib9, BGU_Trumpcon1, BGU_Trumpcon2,
+         BGU_Trumpcon3, BGU_Trumpcon4, BGU_Trumpcon5, BGU_Trumpcon6, BGU_Trumpcon7,
+         BGU_Trumpcon8, BGU_Trumpcon9, BGU_friendlib1, BGU_friendlib2, BGU_friendlib3,
+         BGU_friendlib4, BGU_friendlib5, BGU_friendlib6, BGU_friendlib7, BGU_friendlib8,
+         BGU_friendlib9, BGU_friendcon1, BGU_friendcon2, BGU_friendcon3, BGU_friendcon4,
+         BGU_friendcon5, BGU_friendcon6, BGU_friendcon7, BGU_friendcon8, BGU_friendcon9)
 
-data_new <- data_clean %>%
-  mutate(minimum_wage = coalesce(BGU_control1, BGU_Trumplib1, BGU_Trumpcon1, BGU_friendlib1, BGU_friendcon1),
-         taxes = coalesce(BGU_control2, BGU_Trumplib2, BGU_Trumpcon2, BGU_friendlib2, BGU_friendcon2),
-         abortion = coalesce(BGU_control3, BGU_Trumplib3, BGU_Trumpcon3, BGU_friendlib3, BGU_friendcon3),
-         immigration = coalesce(BGU_control4, BGU_Trumplib4, BGU_Trumpcon4, BGU_friendlib4, BGU_friendcon4),
-         guns = coalesce(BGU_control5, BGU_Trumplib5, BGU_Trumpcon5, BGU_friendlib5, BGU_friendcon5),
-         health = coalesce(BGU_control6, BGU_Trumplib6, BGU_Trumpcon6, BGU_friendlib6, BGU_friendcon6),
-         b_checks = coalesce(BGU_control7, BGU_Trumplib7, BGU_Trumpcon7, BGU_friendlib7, BGU_friendcon7),
-         climate = coalesce(BGU_control8, BGU_Trumplib8, BGU_Trumpcon8, BGU_friendlib8, BGU_friendcon8),
-         p_parent = coalesce(BGU_control9, BGU_Trumplib9, BGU_Trumpcon9, BGU_friendlib9, BGU_friendcon9))
 
-# Check if the variables were properly constructed
-table(data_clean$BGU_control1, data_new$minimum_wage)
-table(data_clean$BGU_Trumplib1, data_new$minimum_wage)
-table(data_clean$BGU_friendlib1, data_new$minimum_wage)
-table(data_clean$BGU_control1, data_new$minimum_wage)
-table(data_clean$BGU_control1, data_new$minimum_wage)
+### Now, let's modify the database so it's in long format
 
-table(data_new$BGU_group)
-
-# 4) Convert the variables of interest to numeric
-data_new %<>%
-  mutate(BGU_group = as.factor(BGU_group),
-         BGU_group = factor(BGU_group, levels = c(1, 2, 3, 4, 5), 
-                            labels = c("control", "TL", "TC", "CFL", "CFC")),
-         minimum_wage = as.numeric(minimum_wage),
-         taxes = as.numeric(taxes),
-         abortion = as.numeric(abortion),
-         immigration = as.numeric(immigration),
-         guns = as.numeric(guns),
-         health = as.numeric(health),
-         b_checks = as.numeric(b_checks),
-         climate = as.numeric(climate),
-         p_parent = as.numeric(p_parent))
-
-# 5) Replace `don't know` (category 9) in policy positions as NAs
-
-data_new %<>%
-  mutate(minimum_wage = replace(minimum_wage, minimum_wage == 9, NA),
-         taxes = replace(taxes, taxes == 9, NA),
-         abortion = replace(abortion, abortion == 9, NA),
-         immigration = replace(immigration, immigration == 9, NA),
-         guns = replace(guns, guns == 9, NA),
-         health = replace(health, health == 9, NA),
-         b_checks = replace(b_checks, b_checks == 9, NA),
-         climate = replace(climate, climate == 9, NA),
-         p_parent = replace(p_parent, p_parent == 9, NA))
-
-table(data_new$minimum_wage, exclude = NULL)
-
-# 6) Replace 1 and 2 policy positions for 0 and 1. 0 for oppose 1 for support
-
-data_new %<>%
-  mutate(
-    minimum_wage = ifelse(minimum_wage == 2, 0, minimum_wage),
-    taxes = ifelse(taxes == 2, 0, taxes),
-    abortion = ifelse(abortion == 2, 0, abortion), #oppose
-    immigration = ifelse(immigration == 2, 0, immigration),
-    guns = ifelse(guns == 2, 0, guns), #oppose
-    health = ifelse(health == 2, 0, health),
-    b_checks = ifelse(b_checks == 2, 0, b_checks),
-    climate = ifelse(climate == 2, 0, climate),
-    p_parent = ifelse(p_parent == 2, 0, p_parent)
+data_long <- data_filtered %>%
+  pivot_longer(
+    cols = c(
+      BGU_control1, BGU_control2, BGU_control3, BGU_control4, BGU_control5,
+      BGU_control6, BGU_control7, BGU_control8, BGU_control9, BGU_Trumplib1,
+      BGU_Trumplib2, BGU_Trumplib3, BGU_Trumplib4, BGU_Trumplib5, BGU_Trumplib6,
+      BGU_Trumplib7, BGU_Trumplib8, BGU_Trumplib9, BGU_Trumpcon1, BGU_Trumpcon2,
+      BGU_Trumpcon3, BGU_Trumpcon4, BGU_Trumpcon5, BGU_Trumpcon6, BGU_Trumpcon7,
+      BGU_Trumpcon8, BGU_Trumpcon9, BGU_friendlib1, BGU_friendlib2, BGU_friendlib3,
+      BGU_friendlib4, BGU_friendlib5, BGU_friendlib6, BGU_friendlib7, BGU_friendlib8,
+      BGU_friendlib9, BGU_friendcon1, BGU_friendcon2, BGU_friendcon3, BGU_friendcon4,
+      BGU_friendcon5, BGU_friendcon6, BGU_friendcon7, BGU_friendcon8, BGU_friendcon9
+    ),
+    names_to = "policy_positions_raw",
+    values_to = "policy_opinion" # shows support oppose 
   )
 
-table(data_new$minimum_wage, exclude = NULL)
+### Create a new policy position variable that stores all the 9 policy positions where:
+# 1 minimum wage 2 taxes 3 abortion 4 immigration 5 guns 6 health care 7 background checks
+# 8 climate change 9 planned parenthood
 
-# 7) Change the order of some policy positions
+data_long <- data_long %>%
+  mutate(policy_position = as.numeric(sub(".*([0-9]+)$", "\\1", policy_positions_raw)))
 
-# For the majority of policy positions, the liberal option is the support option
-# whereas the oppose (2) stands for the conservative one, except in two cases:
-# Abortion: Do you support or oppose enforcing penalties on women who obtain abortions? Liberal position - oppose (2)
-# Guns: Do you support or oppose allowing teachers to carry guns on school property? Liberal position - oppose (2)
+### Modify policy_opinion
+# Following Barber & Pope, they code the policy positions as following:
+# 1 support 0 don't know -1 oppose
+# let's first see how the original variable looks like
 
-# To maintain consistency, the positions will be switched so the lower number also shows the conservative option
-# and the higher number. Reminder: now oppose is 0 based on previous changes.
+table(data_long$policy_opinion)
+#1    2    9 
+#5162 2882  956 
 
-table(data_new$abortion)
-table(data_new$guns)
+# 1 (supports) stay the same. 2 (oppose) becomes -1, and 9 (don't know) becomes 0.
 
-data_new %<>%
-  mutate(abortion_recoded = ifelse(abortion == 0, 1, 0),
-         guns_recoded = ifelse(guns == 0, 1, 0))
+data_long %<>%
+  mutate(policy_opinion = ifelse(policy_opinion == 9, 0, ifelse(policy_opinion == 2, -1, policy_opinion)))
 
-table(data_new$abortion_recoded)
-table(data_new$guns_recoded)
+table(data_long$policy_opinion)
+#-1    0    1 
+#2882  956 5162 
+
+
+### Create experimental treatment conditions based on BGU_group
+table(data_long$BGU_group)
+
+data_long %<>%
+  mutate(control = if_else(BGU_group == 1, 1, 0),
+         libtrump = if_else(BGU_group == 2, 1, 0),
+         contrump = if_else(BGU_group == 3, 1, 0),
+         libfriend = if_else(BGU_group == 4, 1, 0),
+         confriend = if_else(BGU_group == 5, 1, 0))
+
+### Double-check consistency
+table(data_long$control, data_long$BGU_group)
+table(data_long$confriend, data_long$BGU_group)
+
+table(data_long$policy_opinion, data_long$policy_position,  exclude = NULL) # 4000 NAs
+table(data_filtered$BGU_control2, exclude = NULL) #usually around 800 NAs
+table(data_filtered$BGU_friendcon5, exclude = NULL)
+table(data_filtered$BGU_Trumplib9, exclude = NULL)
+table(data_filtered$BGU_Trumplib5, exclude = NULL)
+# Given that there's usually 800 NAS per position, and there are 5 experimental treatments,
+# it's expected that there will be 4000 NAs per policy positio
+
+
+# 4) Data exploration: mean demographic values by treatment assignment
+# Using data_raw, the following vabiables:
+# race: 1 = white
+# gender4: 1 = male
+# BGU_knowledge1 to BGU_knowledge6 for political knowledge
+# BGU_Trumpapproval: 1 = strongly approve 5 = strongly disapprove
+# CC21_330a ideology: 1 = very liberal 7 = very conservative
+# pid3 party id: 1 = democrat 2 = republican 3 = independent
+# faminc_new
+# educ
+
 
 # Save data
-save(data_new, file = "/Users/franciscacastro/Dropbox/Shared_ERC_Francisca_and_Jenny/r-project/data/02-processed-data/data_final.Rdata")
+save(data_long, file = "C:/Users/Francisca/Dropbox/Shared_ERC_Francisca_and_Jenny/r-project/data/02-processed-data/data_long.Rdata")
