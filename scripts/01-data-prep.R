af97214782cf07b5b340a71ac7a81c96f5347f8e
Francisca <frcastrog@uc.cl>
2023-10-14 14:05:41 +0200

    Update Oct 14

--- scripts/01-data-prep.R
+++ scripts/01-data-prep.R
@@ -23,6 +23,24 @@ names(data_raw)
 data_raw %<>%
   mutate_if(is.character, ~ ifelse(. == "__NA__", NA, .)) 
 
+data_raw %<>%
+  mutate(
+    BGU_group = as.numeric(as.character(BGU_group)),
+    race = as.numeric(as.character(race)),
+    gender4 = as.numeric(as.character(gender4)),
+    BGU_Trumpapproval = as.numeric(as.character(BGU_Trumpapproval)),
+    CC21_330a = as.numeric(as.character(CC21_330a)),
+    pid3 = as.numeric(as.character(pid3)),
+    faminc_new = as.numeric(as.character(faminc_new)),
+    educ = as.numeric(as.character(educ)),
+    BGU_conf1 = as.numeric(as.character(BGU_conf1)),
+    BGU_conf2 = as.numeric(as.character(BGU_conf2)),
+    BGU_conf3 = as.numeric(as.character(BGU_conf3)),
+    BGU_conf4 = as.numeric(as.character(BGU_conf4)),
+    BGU_conf5 = as.numeric(as.character(BGU_conf5)),
+    BGU_conf6 = as.numeric(as.character(BGU_conf6))
+  )
+
 ### Keep only necessary variables
 # So far: caseid, teamweight, birthyr, educ, race, CC21_330a (self ideology),
 # pid3 (3 point party ID), pid7 (7 point party id), gender4, BGU_Trumpapproval,
@@ -50,7 +68,8 @@ data_filtered <- data_raw %>%
          BGU_Trumpcon8, BGU_Trumpcon9, BGU_friendlib1, BGU_friendlib2, BGU_friendlib3,
          BGU_friendlib4, BGU_friendlib5, BGU_friendlib6, BGU_friendlib7, BGU_friendlib8,
          BGU_friendlib9, BGU_friendcon1, BGU_friendcon2, BGU_friendcon3, BGU_friendcon4,
-         BGU_friendcon5, BGU_friendcon6, BGU_friendcon7, BGU_friendcon8, BGU_friendcon9)
+         BGU_friendcon5, BGU_friendcon6, BGU_friendcon7, BGU_friendcon8, BGU_friendcon9,
+         BGU_conf1,BGU_conf2,BGU_conf3,BGU_conf4,BGU_conf5,BGU_conf6)
 
 
 ### Now, let's modify the database so it's in long format
@@ -121,17 +140,59 @@ table(data_filtered$BGU_Trumplib5, exclude = NULL)
 # it's expected that there will be 4000 NAs per policy positio
 
 
-# 4) Data exploration: mean demographic values by treatment assignment
-# Using data_raw, the following vabiables:
-# race: 1 = white
-# gender4: 1 = male
-# BGU_knowledge1 to BGU_knowledge6 for political knowledge
-# BGU_Trumpapproval: 1 = strongly approve 5 = strongly disapprove
-# CC21_330a ideology: 1 = very liberal 7 = very conservative
-# pid3 party id: 1 = democrat 2 = republican 3 = independent
-# faminc_new
-# educ
+###############################################################################
 
+# 4) Additional data in wide format (might be useful for testing things)
+
+data_wide <- data_filtered %>%
+  mutate(minimum_wage = coalesce(BGU_control1, BGU_Trumplib1, BGU_Trumpcon1, BGU_friendlib1, BGU_friendcon1),
+         taxes = coalesce(BGU_control2, BGU_Trumplib2, BGU_Trumpcon2, BGU_friendlib2, BGU_friendcon2),
+         abortion = coalesce(BGU_control3, BGU_Trumplib3, BGU_Trumpcon3, BGU_friendlib3, BGU_friendcon3),
+         immigration = coalesce(BGU_control4, BGU_Trumplib4, BGU_Trumpcon4, BGU_friendlib4, BGU_friendcon4),
+         guns = coalesce(BGU_control5, BGU_Trumplib5, BGU_Trumpcon5, BGU_friendlib5, BGU_friendcon5),
+         health = coalesce(BGU_control6, BGU_Trumplib6, BGU_Trumpcon6, BGU_friendlib6, BGU_friendcon6),
+         b_checks = coalesce(BGU_control7, BGU_Trumplib7, BGU_Trumpcon7, BGU_friendlib7, BGU_friendcon7),
+         climate = coalesce(BGU_control8, BGU_Trumplib8, BGU_Trumpcon8, BGU_friendlib8, BGU_friendcon8),
+         p_parent = coalesce(BGU_control9, BGU_Trumplib9, BGU_Trumpcon9, BGU_friendlib9, BGU_friendcon9))
+
+### Convert the variables of interest to numeric
+data_wide %<>%
+  mutate(BGU_group = as.factor(BGU_group),
+         BGU_group = factor(BGU_group, levels = c(1, 2, 3, 4, 5), 
+                            labels = c("control", "TL", "TC", "CFL", "CFC")),
+         minimum_wage = as.numeric(minimum_wage),
+         taxes = as.numeric(taxes),
+         abortion = as.numeric(abortion),
+         immigration = as.numeric(immigration),
+         guns = as.numeric(guns),
+         health = as.numeric(health),
+         b_checks = as.numeric(b_checks),
+         climate = as.numeric(climate),
+         p_parent = as.numeric(p_parent))
+
+### ### Modify policy_opinion
+# Following Barber & Pope, they code the policy positions as following:
+# 1 support 0 don't know -1 oppose
+
+# Example
+table(data_wide$abortion)
+
+data_wide %<>%
+  mutate(minimum_wage = ifelse(minimum_wage == 9, 0, ifelse(minimum_wage == 2, -1, minimum_wage)),
+         taxes = ifelse(taxes == 9, 0, ifelse(taxes == 2, -1, taxes)),
+         abortion = ifelse(abortion == 9, 0, ifelse(abortion == 2, -1, abortion)),
+         immigration = ifelse(immigration == 9, 0, ifelse(immigration == 2, -1, immigration)),
+         guns = ifelse(guns == 9, 0, ifelse(guns == 2, -1, guns)),
+         health = ifelse(health == 9, 0, ifelse(health == 2, -1, health)),
+         b_checks = ifelse(b_checks == 9, 0, ifelse(b_checks == 2, -1, b_checks)),
+         climate = ifelse(climate == 9, 0, ifelse(climate == 2, -1, climate)),
+         p_parent = ifelse(p_parent == 9, 0, ifelse(p_parent == 2, -1, p_parent)))
+
+table(data_wide$abortion)
 
 # Save data
-save(data_long, file = "C:/Users/Francisca/Dropbox/Shared_ERC_Francisca_and_Jenny/r-project/data/02-processed-data/data_long.Rdata")
+save(data_long, file = "~/Dropbox/Shared_ERC_Francisca_and_Jenny/r-project/data/02-processed-data/data_long.Rdata")
+save(data_raw, file = "~/Dropbox/Shared_ERC_Francisca_and_Jenny/r-project/data/02-processed-data/data_raw.Rdata")
+save(data_wide, file = "~/Dropbox/Shared_ERC_Francisca_and_Jenny/r-project/data/02-processed-data/data_wide.Rdata")
+
+
